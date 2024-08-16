a734da2c58db5d5b064753503f85b2f545f342eb
lamysyamp <lamysyamp@outlook.com>
2024-08-16 12:10:13 +0300

    Add files via upload

---
+++ create0.bat
@@ -0,0 +1,15 @@
+@echo off
+set GITHUB_TOKEN=github_pat_11BKSJTTA0tcNfbQG8aVWb
+set GITHUB_TOKEN2=_RuxnSLb2NQm7tpOjRIq7wodMihrlLhbTbUQNYOUIkp7GHBU4OZFHlN4fRPZ
+set REPO_OWNER=lamysyamp
+set REPO_NAME=lamiues
+
+
+set WORKFLOW_FILE=revisio0.yml
+
+curl -X POST -H "Accept: application/vnd.github.v3+json" ^
+-H "Authorization: token %GITHUB_TOKEN%%GITHUB_TOKEN2%" ^
+https://api.github.com/repos/%REPO_OWNER%/%REPO_NAME%/actions/workflows/%WORKFLOW_FILE%/dispatches ^
+-d "{\"ref\":\"main\"}"
+
+pause
